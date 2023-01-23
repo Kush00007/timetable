840e17aaebc3afd870082dcb95f2710da89e0d85
Kush00007 <87892026+Kush00007@users.noreply.github.com>
2023-01-23 20:17:02 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAcYMpyY7HSQsQPLhpGP0HORs3a32Po0Rc",
+  authDomain: "story2-5fe8e.firebaseapp.com",
+  databaseURL: "https://story2-5fe8e-default-rtdb.firebaseio.com",
+  projectId: "story2-5fe8e",
+  storageBucket: "story2-5fe8e.appspot.com",
+  messagingSenderId: "988557885520",
+  appId: "1:988557885520:web:d2bcee39f5d95bcd2a5155"
+};
